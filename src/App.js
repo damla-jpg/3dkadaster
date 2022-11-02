ae86f3d3269301cb317fa15353221977bfaad170
dul800 <78266324+dul800@users.noreply.github.com>
2022-11-02 23:22:50 +0100

    finals

--- src/App.js
+++ src/App.js
@@ -1,46 +1,57 @@
-import React, {  Component } from 'react'
+import React, { Component } from 'react'
 import { OBJLoader } from 'three-stdlib'
 import "./App.css"
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { Object3D } from 'three'
 
-var scene, camera, renderer;
+// Global GL Variables
+var scene, camera, renderer, controls;
+// City reference
 var city1 = new Object3D;
 
+// Init application
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: "/objects/groningen.obj"
+      view: "/objects/amsterdam_ne.obj"
     }
   }
 
+  // On view change (tab changes -> forces model change)
   setView = (city) => {
+    // Set view state
     this.setState({ view: city })
-    console.log(this.state.view)
+    // Clear the scene and add the model to the scene
     scene.clear()
     this.addToScene(this.state.view);
   }
 
+  // Handles the model change
   addToScene = (path) => {
-    var manager = new THREE.LoadingManager();
-    const loader = new OBJLoader(manager);
-    // load a resource
+    const loader = new OBJLoader();
+
+    var city_geometry
+
     loader.load(
-      // resource URL
       path,
-      // called when resource is loaded
+
       function (object) {
+        // Init City
         city1 = object
         scene.add(city1);
         renderer.render(scene, camera);
-        const aabb = new THREE.Box3().setFromObject(object);
-        const center = aabb.getCenter(new THREE.Vector3());
 
-        object.position.x += (object.position.x - center.x + 100);
+        city_geometry = new THREE.Box3().setFromObject(object);
+        const center = city_geometry.getCenter(new THREE.Vector3());
+
+        // Set City Transform
+        object.position.x += (object.position.x - center.x);
         object.position.y += (object.position.y - center.y);
-        object.position.z += (object.position.z - center.z+ 500);
+        object.position.z += (object.position.z - center.z);
+
+
       },
       // called when loading is in progresses
       function (xhr) {
@@ -53,25 +64,39 @@ export default class App extends Component {
     );
 
     const light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
-  
+
     scene.add(light);
   }
 
   componentDidMount() {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(125, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.x = 10
-    camera.position.y = -5;
-    camera.position.z = 5;
-    camera.position.fromArray([-20, 10, -140]);
     renderer = new THREE.WebGLRenderer();
     renderer.setClearColor("#202020");
     renderer.setSize(window.innerWidth, window.innerHeight);
+
+    scene = new THREE.Scene();
+
+    var camera_render_distance = 50000
+    camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, camera_render_distance);
+
+    controls = new OrbitControls(camera, renderer.domElement);
+
+    // Set camera postion
+    camera.position.x = 0;
+    camera.position.y = 0;
+    camera.position.z = 1500;
+
+    // Camera settings
+    controls.enableDamping = false;
+    controls.minAzimuthAngle = 0
+    controls.maxAzimuthAngle = 0
+    controls.panSpeed = 5
+    controls.zoomSpeed = 5
+
+    controls.update()
     this.mount.appendChild(renderer.domElement);
-    var controls = new OrbitControls(camera, renderer.domElement);
 
+    console.log('Cagrildims')
     this.setView(city1);
-
 
     var animate = function () {
       requestAnimationFrame(animate);
@@ -87,11 +112,14 @@ export default class App extends Component {
         this.mount = mount;
       }}>
         <header className='app-header'>Welcome to 3D Kadaster!</header>
+        <p className='note'>Note to user: If the city does not load please wait a little bit. Since rendering city models can be challenging
+          for React it can take a couple minutes to show. You can also simply try to load another city first and then move back to the other.
+          Use right click and/or the mouse wheel to move around the object and left click to look up and down.
+        </p>
         <button className="nav-button" onClick={() => this.setView("/objects/amsterdam_ne.obj")}>Amsterdam North East</button>
         <button className="nav-button" onClick={() => this.setView("/objects/amsterdam_nw.obj")}>Amsterdam North West</button>
         <button className="nav-button" onClick={() => this.setView("/objects/amsterdam_se.obj")}>Amsterdam South East</button>
         <button className="nav-button" onClick={() => this.setView("/objects/amsterdam_sw.obj")}>Amsterdam South West</button>
-        <button className="nav-button" onClick={() => this.setView("/objects/enschede.obj")}>Enschede</button>
         <button className="nav-button" onClick={() => this.setView("/objects/groningen.obj")}>Groningen</button>
         <button className="nav-button" onClick={() => this.setView("/objects/maastricht.obj")}>Maastricht</button>
         <button className="nav-button" onClick={() => this.setView("/objects/rotterdam.obj")}>Rotterdam</button>
